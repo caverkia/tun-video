0dd824c3f1ab824b3bd68813e5b1ebe30ac68ef5
caverkia <wuhao.thu@gmail.com>
2017-05-12 11:20:35 +0800

    change receiver run.sh

--- run_receiver.sh
+++ run_receiver.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-localip=192.168.1.128
+localip=192.168.1.187
 localport=8888
 
-remoteip=192.168.1.187
+remoteip=192.168.1.128
 remoteport=8888
 
-dev=tun1
+dev=tun2
 
 ./tunudp ${dev} ${localip} ${localport} ${remoteip} ${remoteport}
 
